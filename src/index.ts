7d74066663bc533283003382f4f0b55ab4dfbbfa
Damien Butt <damien.butt@norgateav.com>
2021-06-23 23:56:02 +0100

    test: add Jest config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-const func = (value: string): void => {
-	console.log(value);
-};
+export const pkgAddProp = (
+	pkgPath: string,
+	{ key, value }: { key: string; value: string }
+): void => {
+	let pkg = null;
 
-export default func;
+	try {
+		pkg = require(pkgPath); // eslint-disable-line global-require
+	} catch (error) {
+		console.error(`Unable to locate ${pkgPath}`);
+		process.exit();
+	}
+
+	const fs = require("fs"); // eslint-disable-line global-require
+
+	if (pkg[key] !== undefined) {
+		console.log(`Property with key "${key}" is already defined with value : "${value}"`);
+		process.exit();
+	}
+
+	console.log(`Adding property "${key}": "${value}" to ${pkgPath}`);
+
+	pkg[key] = value;
+	fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+};
